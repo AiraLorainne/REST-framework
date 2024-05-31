2d6caa8ddeae434c682c46bb6ccfbc8315007988
AiraLorainne <torres.al.bsinfotech@gmail.com>
2024-05-31 13:57:28 +0800

    app

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.items),
-    path('add/', views.addItem)
+    path('', views.getData),
+    path('add/', views.addItem),
 ]
--- api/views.py
+++ api/views.py
@@ -1,18 +1,19 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Item 
-from .serializer import ItemSeriealizer
+from .serializer import ItemSerializer
 # Create your views here.
 
 
 @api_view(['GET'])
-def items(request):
-    all_items = Item.objects.all()
-    serial = ItemSeriealizer(all_items, many=True)
+def getData(request):
+    items = Item.objects.all()
+    serial = ItemSerializer(items, many=True)
     return Response(serial.data)
+
 @api_view(['POST'])
 def addItem(request):
-    serial = ItemSeriealizer(data=request.data)
+    serial = ItemSerializer(data=request.data)
     if serial.is_valid():
         serial.save()
     return Response(serial.data)
